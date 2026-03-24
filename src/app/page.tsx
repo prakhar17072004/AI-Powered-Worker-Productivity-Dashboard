d8d5296ceee5ceff7b054a1b80a7603ac32f2090
prakhar17072004 <pmankur2004@gmail.com>
2026-03-24 23:00:16 +0530

    css is added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-brfrom-blue-200 to-gray-200 px-6">
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-brfrom-blue-300 to-gray-200 px-6">
       <div className="bg-white shadow-xl rounded-2xl p-8 text-center max-w-md w-full">
         <h1 className="text-3xl font-bold text-gray-800 mb-6">
           AI Productivity Dashboard
