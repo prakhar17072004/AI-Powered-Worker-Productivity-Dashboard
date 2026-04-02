a451f68751f149c5a83a17735daa6cf194e3b931
prakhar17072004 <pmankur2004@gmail.com>
2026-04-02 23:08:09 +0530

    css added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-brfrom-blue-200 to-gray-700 px-6">
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-brfrom-blue-600 to-gray-700 px-6">
       <div className="bg-white shadow-xl rounded-2xl p-8 text-center max-w-md w-full">
         <h1 className="text-3xl font-bold text-gray-700 mb-7">
           AI Productivity Dashboard
@@ -12,7 +12,7 @@ export default function Home() {
 
         <a
           href="/dashboard"
-          className="inline-block bg-blue-700 hover:bg-blue-700 text-white font-medium px-6 py-3 rounded-lg transition duration-300"
+          className="inline-block bg-blue-700 hover:bg-blue-700 text-white font-medium px-6 py-4 rounded-lg transition duration-300"
         >
           Go to Dashboard →
         </a>
